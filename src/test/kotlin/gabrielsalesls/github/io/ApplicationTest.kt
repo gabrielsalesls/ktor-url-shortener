422e0692941b77bbd8cfb070b3a3988411602dba
gabrielsalesls <salesgabriel311@gmail.com>
2023-08-30 10:21:47 -0300

    test: adding integration tests with h2

--- src/test/kotlin/gabrielsalesls/github/io/ApplicationTest.kt
+++ src/test/kotlin/gabrielsalesls/github/io/ApplicationTest.kt
@@ -1,9 +1,14 @@
 package gabrielsalesls.github.io
 
+import gabrielsalesls.github.io.model.request.UrlRequest
 import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
 import io.ktor.serialization.gson.*
 import io.ktor.server.config.*
 import io.ktor.server.testing.*
+import org.junit.Assert
 import org.junit.Test
 
 
@@ -21,6 +26,20 @@ class ApplicationTest {
             followRedirects = true
         }
 
+        client.post("/") {
+            contentType(ContentType.Application.Json)
+            setBody(UrlRequest("www.google.com"))
+        }.let {
+            println(it.bodyAsText())
+            Assert.assertTrue(it.bodyAsText().contains("google"))
+            Assert.assertEquals(HttpStatusCode.Created.value, it.status.value)
+        }
+
+        client.get("/123") {
+            contentType(ContentType.Application.Json)
+        }.let {
+            Assert.assertEquals(HttpStatusCode.NotFound.value, it.status.value)
+        }
 
     }
 }
